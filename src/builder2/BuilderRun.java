038932b330aa52baecfc93b7b7e752ff56ce51f8
Vusal <143846119+GuliyevVusal@users.noreply.github.com>
2024-02-20 22:55:10 +0400

    Update BuilderRun.java

--- src/builder2/BuilderRun.java
+++ src/builder2/BuilderRun.java
@@ -1,2 +1,20 @@
-package builder2;public class BuilderRun {
+package builder2;
+
+public class BuilderRun {
+
+    public static void main(String[] args) {
+
+        Product product1 = new Product();
+        product1.setId(12L);
+        product1.setName("Iphone");
+        product1.setDescription("promax");
+        product1.setPrice(300);
+
+        Product product2 = new Product.ProductBuilder().id(12L).name("Iphone").description("promax").price(100).build();
+
+        System.out.println(product1);
+        System.out.println();
+        System.out.println(product2);
+    }
 }
+
